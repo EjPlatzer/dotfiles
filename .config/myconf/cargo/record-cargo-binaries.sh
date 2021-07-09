edf38b9412e5cb08a5b63e540354955855b2a2ae
Evan Platzer <evan.platzer@gmail.com>
2021-07-09 17:05:08 -0400

    Fix recording cargo binaries when record already exists

--- .config/myconf/cargo/record-cargo-binaries.sh
+++ .config/myconf/cargo/record-cargo-binaries.sh
@@ -5,6 +5,11 @@
 
 filepath=${XDG_HOME_CONFIG:-$HOME/.config}/myconf/cargo/cargo-binaries.cfg
 
+if [ -f $filepath ]; then
+	echo backing up previous cargo binaries
+	mv $filepath $filepath.bak
+fi
+
 echo "Saving list of cargo binaries installed to $filepath"
 
 cargo install --list | sd '([\w-]) v.+\n    [\w-]+' '$1' >> $filepath
