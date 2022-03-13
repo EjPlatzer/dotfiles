c17cad145c82907ecb4a1f7fc3f3a37168bac497
Evan Platzer <evan.platzer@gmail.com>
2022-03-13 14:55:18 -0400

    Switch from Alacritty to wezterm on *nix

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm';
+
+return {
+    font = wezterm.font("FiraCode Nerd Font"),
+    color_scheme = "Dracula+",
+    default_prog = {"/Users/evan/Desktop/nushell_0.59/nu"}
+}
