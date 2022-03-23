0a1b8eef7e381dadcb42eb512b2946eff74ec6a5
Evan Platzer <evan.platzer@gmail.com>
2022-03-23 11:31:54 -0400

    Bump nushell to 0.60 pending nix update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,5 +3,5 @@ local wezterm = require 'wezterm';
 return {
     font = wezterm.font("FiraCode Nerd Font"),
     color_scheme = "Dracula+",
-    default_prog = {"/Users/evan/Desktop/nushell_0.59/nu"}
+    default_prog = {"/Users/evan/Desktop/nushell-0.60.0/nu"}
 }
