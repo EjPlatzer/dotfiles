584cfb7398bd9b20e8fe441cecfc2b4bfaad10ff
Evan Platzer <evan.platzer@gmail.com>
2022-04-14 16:02:28 -0400

    Remove wezterm

--- .config/wezterm/wezterm.lua
+++
@@ -1,7 +0,0 @@
-local wezterm = require 'wezterm';
-
-return {
-    font = wezterm.font("FiraCode Nerd Font"),
-    color_scheme = "Dracula+",
-    default_prog = {"/Users/evan/Desktop/nushell-0.60.0/nu"}
-}
